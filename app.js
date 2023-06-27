99896e5c5b17b2637811fcf9015719ddde0cdfaa
Bryan King Pecho <bkpecho05@gmail.com>
2023-06-27 16:32:30 +0800

    feat(app): Initial setup and basic routing

---
+++ app.js
@@ -0,0 +1,27 @@
+require('dotenv').config();
+
+const express = require('express');
+const expressLayout = require('express-ejs-layouts');
+
+const app = express();
+const PORT = 3000 || process.env.PORT;
+
+// parse data from forms
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// serve static files from Public folder
+app.use(express.static('public'));
+
+// templating engine
+app.use(expressLayout);
+app.set('layout', './layouts/main');
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+  res.send('Hello World');
+});
+
+app.listen(PORT, () => {
+  console.log(`App is listening on port ${PORT} 🎉🎉🎉`);
+});
