a788e39c570dd45035c06675930d3ac432676282
Bryan King Pecho <bkpecho05@gmail.com>
2023-06-27 22:22:23 +0800

    feat(CustomerRoutes): Update customer.js file with additional routes

--- server/routes/customer.js
+++ server/routes/customer.js
@@ -5,5 +5,8 @@ const customerController = require('../controllers/customerController');
 /* Customer Routes */
 router.get('/', customerController.homepage);
 
+router.get('/add', customerController.addCustomer);
+router.post('/add', customerController.postCustomer);
+
 // it is important to export the router
 module.exports = router;
