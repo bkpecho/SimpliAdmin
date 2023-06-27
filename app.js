e11a0a7ef30ca5ab45cdb015be5ad8b4cbf0260c
Bryan King Pecho <bkpecho05@gmail.com>
2023-06-27 18:55:03 +0800

    feat(App): Update app.js with database connection and customer routes

--- app.js
+++ app.js
@@ -2,9 +2,15 @@ require('dotenv').config();
 
 const express = require('express');
 const expressLayout = require('express-ejs-layouts');
+const connectDB = require('./server/config/db');
 
 const app = express();
 const PORT = 3000 || process.env.PORT;
+
+// connect to database
+connectDB();
+
+const customerRoutes = require('./server/routes/customer');
 
 // parse data from forms
 app.use(express.urlencoded({ extended: true }));
@@ -18,16 +24,14 @@ app.use(expressLayout);
 app.set('layout', './layouts/main');
 app.set('view engine', 'ejs');
 
-// home route
-app.get('/', (req, res) => {
-  const locals = {
-    title: 'NodeJS',
-    description: 'Free NodeJS User Management System'
-  };
+// routes
+app.use('/', customerRoutes);
 
-  res.render('index', { locals });
+// handle 404
+app.get('*', (req, res) => {
+  res.status(404).render('404');
 });
 
 app.listen(PORT, () => {
-  console.log(`App is listening on port ${PORT} 🎉🎉🎉`);
+  console.log(`🆗🆗🆗 App is listening on port ${PORT}`);
 });
