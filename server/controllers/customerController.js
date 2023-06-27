86cc33c0f711261a88c85737c3d03606bc215c8a
Bryan King Pecho <bkpecho05@gmail.com>
2023-06-28 00:30:22 +0800

    feat(CustomerController): Add route for viewing a customer with ID and viewCustomer controller function

--- server/controllers/customerController.js
+++ server/controllers/customerController.js
@@ -4,13 +4,32 @@ const Customer = require('../models/Customer');
 // get homepage
 exports.homepage = async (req, res) => {
   const messages = await req.consumeFlash('info');
-
   const locals = {
     title: 'NodeJs',
     description: 'Free NodeJS User Management System'
   };
 
-  res.render('index', { locals, messages });
+  let perPage = 12;
+  let page = req.query.page || 1;
+
+  try {
+    const customers = await Customer.aggregate([{ $sort: { updatedAt: -1 } }])
+      .skip(perPage * page - perPage)
+      .limit(perPage)
+      .exec();
+
+    const count = await Customer.count();
+
+    res.render('index', {
+      locals,
+      customers,
+      current: page,
+      pages: Math.ceil(count / perPage),
+      messages
+    });
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 // get - new customer form
@@ -49,5 +68,20 @@ exports.postCustomer = async (req, res) => {
     res.redirect('/');
   } catch (error) {
     console.log(`🔴🔴🔴 New Customer Creation Failed: ${error}`);
+  }
+};
+
+exports.viewCustomer = async (req, res) => {
+  try {
+    const customer = await Customer.findOne({ _id: req.params.id });
+
+    const locals = {
+      title: 'View Customer Data',
+      description: 'Free NodeJS User Management System'
+    };
+
+    res.render('customer/view', { locals, customer });
+  } catch (error) {
+    console.log(`🔴🔴🔴 Customer Data Loading Failed: ${error}`);
   }
 };
