4ffdcc26ebc4e2febf0e0e3e303ce4cc018d8998
Bryan King Pecho <bkpecho05@gmail.com>
2023-06-28 00:29:47 +0800

    feat(Customer): Add route for viewing a customer with ID

--- server/routes/customer.js
+++ server/routes/customer.js
@@ -8,5 +8,7 @@ router.get('/', customerController.homepage);
 router.get('/add', customerController.addCustomer);
 router.post('/add', customerController.postCustomer);
 
+router.get('/view/:id', customerController.viewCustomer);
+
 // it is important to export the router
 module.exports = router;
